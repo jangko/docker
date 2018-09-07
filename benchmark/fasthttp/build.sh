0197895b62aafbb129a64bfc535cb79fecad4420
jangko <jangko128@gmail.com>
2018-09-07 21:01:06 +0700

    fix GOROOT

--- benchmark/fasthttp/build.sh
+++ benchmark/fasthttp/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 INSDIR=$PWD/install
 GOPATH=$PWD
+GOROOT=$PWD/istall/go
 
 if [ ! -f server ]; then
   echo "building fasthttp..."
@@ -8,8 +9,9 @@ if [ ! -f server ]; then
   wget -q https://dl.google.com/go/go1.11.linux-amd64.tar.gz
   tar -C $INSDIR -xzf go1.11.linux-amd64.tar.gz
   mkdir bin
-  export PATH=$PATH:$INSDIR/go/bin:$GOPATH/bin
+  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
   export GOPATH=$GOPATH
+  export GOROOT=$GOROOT
   go get -d -u github.com/valyala/fasthttp/...
   go build -gcflags='-l=4' server
 fi
